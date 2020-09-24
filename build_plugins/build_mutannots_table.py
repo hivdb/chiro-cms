e43cdd242baaeebf0dc5c6e291783c0b861ff7ae
Philip Tzou <philip.npc@gmail.com>
2020-09-23 21:32:34 -0700

    Add mutannot-spike-table

---
+++ build_plugins/build_mutannots_table.py
@@ -0,0 +1,123 @@
+import os
+import re
+import json
+import ruamel.yaml
+
+yaml = ruamel.yaml.YAML()
+
+VERSION = '20200910120644'
+
+
+def get_citation_id(citation, reverse_citations):
+    doi = citation['doi']
+    section = citation['section']
+    reverse_citation = reverse_citations.setdefault(doi, {
+        'idx': len(reverse_citations) + 1,
+        'sections': {}
+    })
+    citation_id = reverse_citation['idx']
+    section_lookup = reverse_citation['sections']
+    section_id = section_lookup.setdefault(section, 1)
+    return {
+        'citationId': citation_id,
+        'sectionId': section_id
+    }
+
+
+def build_mutannots_table(resource_dir, buildres_dir, **kw):
+    mutannots_dir = os.path.join(resource_dir, 'mutannots')
+    for resyaml in os.listdir(mutannots_dir):
+        resyaml_path = os.path.join(mutannots_dir, resyaml)
+        match = re.search(r'(.+)-config\.ya?ml$', resyaml)
+        if not os.path.isfile(resyaml_path) or not match:
+            continue
+        with open(resyaml_path, encoding='utf-8-sig') as fp:
+            geneconfig = yaml.load(fp)
+        refseq = geneconfig['refSequence']
+
+        resname = match.group(1)
+        genedir = os.path.join(mutannots_dir, resname)
+        annotdata_lookup = {}
+        for annotyaml in os.listdir(genedir):
+            with open(os.path.join(genedir, annotyaml),
+                      encoding='utf-8-sig') as fp:
+                annotdata = yaml.load(fp)
+                annotdata_lookup[annotdata['name']] = annotdata
+        coldefs = [
+            {'name': 'position',
+             'label': 'Pos',
+             'sort': 'numeric'},
+            {'name': 'refAA',
+             'label': 'Ref'}
+        ]
+        rows = {}
+        for cat in geneconfig['annotCategories']:
+            cat_annots = cat.pop('annotNames')
+            for annot_name in cat_annots:
+                annotdata = annotdata_lookup[annot_name]
+                annot_label = annotdata.get(
+                    'tableColLabel',
+                    annotdata.get('label', annot_name)
+                )
+                level = annotdata['level']
+
+                pos_by_cite = [
+                    set(cite['positions']) for cite in annotdata['citations']
+                ]
+
+                if all(pos == pos_by_cite[0] for pos in pos_by_cite):
+                    pos_by_cite = pos_by_cite[:1]
+                    col_names = [annot_label]
+                    coldefs.append({
+                        'name': annot_label,
+                        'label': annot_label,
+                        'none': ''
+                    })
+                else:
+                    col_names = [
+                        '{}-{author}-{year}'.format(annot_label, **cite)
+                        for cite in annotdata['citations']
+                    ]
+                    coldefs.extend([{
+                        'name': ('{}-{author}-{year}'
+                                 .format(annot_label, **cite)),
+                        'label': ('{}\n({}{})'
+                                  .format(annot_label,
+                                          cite['author'],
+                                          cite['year'] - 2000)),
+                        'none': ''
+                    } for cite in annotdata['citations']])
+
+                for col_name, posset in zip(col_names, pos_by_cite):
+                    if level == 'position':
+                        for subgroup in annotdata['positions']:
+                            for pos in subgroup['positions']:
+                                if pos not in posset:
+                                    continue
+                                row = rows.setdefault(pos, {
+                                    'position': pos,
+                                    'refAA': refseq[pos - 1]
+                                })
+                                row[col_name] = subgroup['subgroup']
+                    else:
+                        for aas in annotdata['aminoAcids']:
+                            match = re.match(r'^(\d+)([A-Zid*]+)$', aas)
+                            pos, aas = match.groups()
+                            pos = int(pos)
+                            if pos not in posset:
+                                continue
+                            row = rows.setdefault(pos, {
+                                'position': pos,
+                                'refAA': refseq[pos - 1]
+                            })
+                            row[col_name] = ''.join(sorted(aas))
+
+        rows = list(rows.values())
+        dest_json = os.path.join(
+            buildres_dir, 'mutannot-{}-table.json'.format(resname))
+        with open(dest_json, 'w') as fp:
+            json.dump({
+                'columnDefs': coldefs,
+                'data': rows
+            }, fp, indent=2)
+            print('create: {}'.format(dest_json))
