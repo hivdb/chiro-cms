857103e74ee8a2ba93ff012925712099b3484e1a
Kaiming <mads396@gmail.com>
2021-02-17 16:47:20 -0800

    update susc-tables

--- scripts/convert_susc_table.sh
+++ scripts/convert_susc_table.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+cp ../covid-drdb-reports/susceptibility-data_files/table/*.json ./resources/susc-table/
+pipenv run python ./scripts/json2yaml.py ./resources/susc-table/table3.json resources/susc-table/table3.yml
+pipenv run python ./scripts/json2yaml.py ./resources/susc-table/table4.json resources/susc-table/table4.yml
 pipenv run python ./scripts/json2yaml.py ./resources/susc-table/tableS4.json resources/susc-table/tableS4.yml
 pipenv run python ./scripts/json2yaml.py ./resources/susc-table/tableS5.json resources/susc-table/tableS5.yml
 pipenv run python ./scripts/json2yaml.py ./resources/susc-table/tableS6.json resources/susc-table/tableS6.yml
