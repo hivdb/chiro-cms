2215f2860339607cc9d0c559008c5952ef091eba
Kaiming <mads396@gmail.com>
2021-03-19 18:48:18 -0700

    add reference for susceptibility page.

---
+++ scripts/convert_susc_table.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+import click
+import json
+
+
+@click.command()
+@click.argument('input_file', type=click.Path())
+@click.argument('output_file', type=click.Path())
+def convert_susc_table(input_file, output_file):
+    with open(input_file, encoding='utf-8') as fd:
+        data = json.load(fd)
+    result = []
+    for item in data:
+        if not item.get('assays'):
+            result.append(item)
+            continue
+
+        new_assays = []
+        for rec in item.get('assays'):
+            refname = rec['reference']
+            refID = refname.replace('*', '')
+            rec['reference'] = "{}[^{}]".format(refname, refID)
+            new_assays.append(rec)
+        item['assays'] = new_assays
+        result.append(item)
+
+
+    with open(output_file, 'w', encoding='utf-8') as fd:
+        json.dump(result, fd, indent=4, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    convert_susc_table()
--- scripts/convert_susc_table.sh
+++ scripts/convert_susc_table.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 cp ../covid-drdb-reports/susceptibility-data_files/table/*.json ./resources/susc-table/
+pipenv run python ./scripts/convert_susc_table.py ./resources/susc-table/table3.json ./resources/susc-table/table3.json
+pipenv run python ./scripts/convert_susc_table.py ./resources/susc-table/table4.json ./resources/susc-table/table4.json
+pipenv run python ./scripts/convert_susc_table.py ./resources/susc-table/tableS3.json ./resources/susc-table/tableS3.json
+pipenv run python ./scripts/convert_susc_table.py ./resources/susc-table/tableS4.json ./resources/susc-table/tableS4.json
+pipenv run python ./scripts/convert_susc_table.py ./resources/susc-table/tableS5.json ./resources/susc-table/tableS5.json
+pipenv run python ./scripts/convert_susc_table.py ./resources/susc-table/tableS6.json ./resources/susc-table/tableS6.json
+pipenv run python ./scripts/convert_susc_table.py ./resources/susc-table/tableS7.json ./resources/susc-table/tableS7.json
+
+
 pipenv run python ./scripts/json2yaml.py ./resources/susc-table/table3.json resources/susc-table/table3.yml
 pipenv run python ./scripts/json2yaml.py ./resources/susc-table/table4.json resources/susc-table/table4.yml
 pipenv run python ./scripts/json2yaml.py ./resources/susc-table/tableS3.json resources/susc-table/tableS3.yml
