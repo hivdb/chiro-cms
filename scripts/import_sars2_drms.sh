33ebae1e79c26b96b3ca9e4d850712ebe56e204f
Philip Tzou <philip.npc@gmail.com>
2022-03-02 14:32:17 -0800

    Update resistance mutations

--- scripts/import_sars2_drms.sh
+++ scripts/import_sars2_drms.sh
@@ -21,44 +21,50 @@ SELECT
     ELSE im.amino_acid
   END AS aa
 FROM isolate_mutations im
-  JOIN isolate_pairs ip ON
-    im.iso_name = ip.iso_name
   JOIN isolates iso ON
     im.iso_name = iso.iso_name
-  JOIN susc_results s ON
-    s.control_iso_name=ip.control_iso_name AND
-    s.iso_name = ip.iso_name
 WHERE
   (
+    (position=444 AND amino_acid='E') OR -- pending for entering: FDA21c
+    (position=450 AND amino_acid='K') OR -- pending for entering: FDA21c
     (
-      ip.num_mutations = 1 AND
-      fold >= 5
+      TRUE AND
+      EXISTS (
+        SELECT 1
+        FROM isolate_pairs ip, susc_results s, rx_antibodies rxab, antibodies ab
+        WHERE
+          im.iso_name = ip.iso_name AND
+          s.control_iso_name=ip.control_iso_name AND
+          s.iso_name = ip.iso_name AND
+          s.ref_name=rxab.ref_name AND
+          s.rx_name=rxab.rx_name AND
+          rxab.ab_name=ab.ab_name AND
+          ip.num_mutations = 1 AND
+          fold >= 5 AND
+          visibility = 1
+      ) AND
+      (
+        iso.var_name IS NULL OR
+        iso.var_name NOT IN (
+          'SARS-CoV-1', 'WIV1', 'pCoV-GD', 'pCoV-GX', 'bCoV-RaTG13', 'Unknown Variant'
+        )
+      )
     ) OR
     EXISTS (
       SELECT 1
-      FROM antibody_epitopes abe, antibodies ab
+      FROM dms_escape_results dms, rx_antibodies rxab, antibodies ab
       WHERE
-        abe.ab_name=ab.ab_name AND
-        ab.visibility=1 AND
-        abe.position=im.position
+        dms.gene=im.gene AND
+        dms.position=im.position AND
+        dms.amino_acid=im.amino_acid AND
+        escape_score>=0.1 AND
+        dms.ref_name=rxab.ref_name AND
+        dms.rx_name=rxab.rx_name AND
+        rxab.ab_name=ab.ab_name AND
+        ab.visibility=1
     )
-  ) AND
-  EXISTS (
-    SELECT 1
-    FROM rx_antibodies rxab, antibodies ab
-    WHERE
-      s.ref_name=rxab.ref_name AND
-      s.rx_name=rxab.rx_name AND
-      rxab.ab_name=ab.ab_name AND
-      visibility = 1
   ) AND
   im.gene = 'S' AND
-  (
-    iso.var_name IS NULL OR 
-    iso.var_name NOT IN (
-      'SARS-CoV-1', 'WIV1', 'pCoV-GD', 'pCoV-GX', 'bCoV-RaTG13', 'Unknown Variant'
-    )
-  ) AND
   NOT EXISTS (
     SELECT 1
     FROM ignore_mutations igm
@@ -71,6 +77,9 @@ GROUP BY im.gene, im.position, aa
 ORDER BY im.gene, im.position, aa;
 .quit
 EOF
+if [ "$(wc -l "$(realpath downloads/resistance-mutations)/latest.tsv" | awk '{print $1}')" -eq "0" ]; then
+  exit 1
+fi
 echo "Create: $(realpath downloads/resistance-mutations)/latest.tsv"
 
 cat <<EOF | python3
