3a9ef73cd0f6d259bbb1d8d19696e0fa75411865
Philip Tzou <philip.npc@gmail.com>
2022-06-13 19:47:52 -0700

    Add YAML file for variants page

--- build.py
+++ build.py
@@ -50,6 +50,7 @@ def load_resources(data):
     if isinstance(data, dict):
         if '_resource' in data:
             rpath = data.pop('_resource')
+            key = data.pop('_resource_key', None)
             resource_path = os.path.join(BUILDRESDIR, rpath)
             if not os.path.isfile(resource_path):
                 resource_path = os.path.join(RESOURCEDIR, rpath)
@@ -61,6 +62,8 @@ def load_resources(data):
                     data = yaml.load(fp)
                 else:
                     data = fp.read()
+            if key:
+                data = data[key]
         else:
             for key, val in list(data.items()):
                 val, nested_mtime = load_resources(val)
