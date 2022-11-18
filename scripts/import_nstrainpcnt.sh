e4adf73dd5bd662eafef99e607dec7e6afd44891
Philip Tzou <philip.npc@gmail.com>
2022-11-17 17:05:19 -0800

    Update nstrainpcnt

--- scripts/import_nstrainpcnt.sh
+++ scripts/import_nstrainpcnt.sh
@@ -8,9 +8,8 @@ mkdir -p local/cache-nstrain-aapcnt/
 pushd local/cache-nstrain-aapcnt/
 
 rm global.json.gz global-frequencies.json global.json 2> /dev/null || true
-curl -sSLR -o global.json.gz "https://nextstrain.org/charon/getDataset?prefix=/ncov/global"
-curl -sSLR -o global-frequencies.json "https://nextstrain.org/charon/getDataset?prefix=/ncov/global&type=tip-frequencies"
-gzip -d global.json.gz
+curl -sSLR -o global.json "https://nextstrain.org/charon/getDataset?prefix=ncov/gisaid/global/6m"
+curl -sSLR -o global-frequencies.json "https://nextstrain.org/charon/getDataset?prefix=ncov/gisaid/global/6m&type=tip-frequencies"
 
 popd
 pipenv run python ./scripts/import_nstrainpcnt.py RdRP S
